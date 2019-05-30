df20587484b2289e89ab52080a612afdffc02457
bugithubUser <43409097+bugithubUser@users.noreply.github.com>
2019-05-30 12:02:53 +0800

    修改错误、添加功能、优化csv编码
    1.修改错误
    2.新增功能：添加csv源码、添加csv文件（其实只是多了个读取文件的过程）
    3.优化csv文件编码方式，不再在行末添加逗号，不再一股脑的用双引号包含数据，减小体积

--- csv.js
+++ csv.js
@@ -1,6 +1,10 @@
 function Csv()
 {
 	var csvTable=[];
+	var tempCsvInput=document.createElement('input');
+	tempCsvInput.style.display='none';
+	tempCsvInput.type='file';
+
 	// 添加值
 	this.add=function(value,y=csvTable.length-1<0?0:csvTable.length-1)
 	{
@@ -13,20 +17,64 @@ function Csv()
 		csvTable.push(array);
 	}
 	// 添加表格
-	this.addHTMLTable=function(tableTrait,getValueFun=function(td){return td.innerText})
+	this.addHTMLTable=function(tableTrait,getValueFun=function(T){return T.innerText})
 	{
 		var targetTable=typeof(tableTrait)=='string'?document.querySelector(tableTrait):tableTrait;
 		var currentCsv=this;
 		var trs=targetTable.getElementsByTagName('tr');
 		[].forEach.call(trs,function(trE,trI,trA)
 		{
-			console.log(trI)
 			var tempArray=[];
 			[].forEach.call(trE.children,function(tdE,tdI,tdA)
 			{
 				tempArray.push(getValueFun(tdE));
 			});
 			currentCsv.addRow(tempArray);
+		});
+	}
+	//添加csv文件
+	this.addCsvFile=function()
+	{
+		var csvContent='';
+		var readCsvFun=(function *(thisCsvObject)
+		{
+			tempCsvInput.onchange=function()
+			{
+				if(tempCsvInput.value!='')
+				{
+					var readCsv=new FileReader();
+					readCsv.onload=function(event)
+					{
+						csvContent=event.target.result;
+						readCsvFun.next();
+					};
+					readCsv.readAsText(tempCsvInput.files[0]);
+				}
+				tempCsvInput.remove();
+			};
+			document.body.append(tempCsvInput);
+			tempCsvInput.click();
+			yield;
+			thisCsvObject.addCsvCode(csvContent,thisCsvObject);
+		})(this);
+
+		readCsvFun.next();
+	}
+	//解析并添加csv代码
+	this.addCsvCode=function(csvCode,thisCsvObject)
+	{
+		var grids=csvCode.match(/(?<=^|,)(([^\n,"]*)|("([^"]*("")*)*"))(?=$|,)\n?/gm);
+		var tempArray=[];
+		grids.forEach(function(V,I,A)
+		{
+			if(V.charAt(V.length-1)=='\n') V=V.slice(0,-1);
+			if(V.charAt(0)=='"') V=V.slice(1,-1).replace(/(""){1}?/g,'"');
+			tempArray.push(V);
+			if(grids[I].charAt(grids[I].length-1)=='\n'||I==A.length-1)
+			{
+				thisCsvObject.addRow(tempArray);
+				tempArray=[];
+			}
 		});
 	}
 	// 移除
@@ -53,9 +101,10 @@ function Csv()
 		return csvTable[y][x];
 	}
 	// 获取csv源码
-	this.getContext=function()
+	this.getContent=function()
 	{
 		var context='';
+		tempCsvInput.remove();
 		for(var i=0;i<csvTable.length;i++)
 		{
 			if(csvTable[i]!==undefined)
@@ -66,7 +115,7 @@ function Csv()
 					else context+=',';
 				}
 			}
-			context+="\n";
+			context=context.replace(/,(?=$)/,"\n");
 		}
 		return context;
 	}
@@ -76,7 +125,7 @@ function Csv()
 		var content='';
 		csvArray.forEach(function(E,I,A)
 		{
-			content+=E.getContext();
+			content+=E.getContent();
 		});
 		var tempDownloadLink=document.createElement('a');
 		tempDownloadLink.style.display='none';
@@ -89,6 +138,6 @@ function Csv()
 	// 转义处理
 	var csvEscape=function(text)
 	{
-		return '"'+(text+'').replace(/"/g,'""')+'"';
+		return /[\n,"]+?/.test(text)?'"'+(text+'').replace(/"/g,'""')+'"':text;
 	}
 }
